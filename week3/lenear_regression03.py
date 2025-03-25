a853a6b4540a0bbfa0d49caf3cc4d01c351e9aaf
dan <boyarsp3@gmail.com>
2025-03-25 17:28:18 +0900

    flask

--- week3/lenear_regression03.py
+++ week3/lenear_regression03.py
@@ -1,11 +1,10 @@
 from sklearn.linear_model import LinearRegression
 import pickle
-import pands as pd
+import pandas as pd
 
 # 파일에서 모델 로드
 with open('./manhattan_model.pkl', 'rb') as f:
     model = pickle.load(f)
-
 print(model.coef_)
 print(model.intercept_)
 sample = pd.DataFrame()
